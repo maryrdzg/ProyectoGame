1205feb96e570a691371365320060d4a9aee9bbf
HugoIPDev <bairon_myh@yahoo.com.mx>
2018-04-06 07:53:39 -0500

    Commit whit unity project setup

---
+++ GameDev/Assets/Particula.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particula : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject,10.0f);
+		
+	}
+	
+	// Update is called once per frames
+	void Update () {
+		
+	}
+}
