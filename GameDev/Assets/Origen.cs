27f525a4b3e988e1a753ea79ecc69c01fbf56212
HugoIPDev <bairon_myh@yahoo.com.mx>
2018-04-10 09:26:56 -0500

    Add example of move by code

--- GameDev/Assets/Origen.cs
+++ GameDev/Assets/Origen.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Origen : MonoBehaviour {
 	public GameObject particula;
+	public int dir;
 	// Use this for initialization
 	void Start () {
-		
+		dir = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Instantiate (particula);
+		//Creamos nuevas particulas en la misma posicion del origen
+		Instantiate (particula,this.transform.position,Quaternion.identity);
+		//mover el origen con codigo
+		if (this.transform.position.x > 100.0f) {
+			dir = -1;
+		} else {
+			if(this.transform.position.x < -100.0f)
+			{
+				dir = 1;
+			}
+		}
+		transform.Translate (Vector3.right * (Time.deltaTime * 500.0f*dir));
 	}
 }
