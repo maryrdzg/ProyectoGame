308962b9bd4f5060677d5ca9ca46ef854b67e651
HugoIPDev <bairon_myh@yahoo.com.mx>
2018-04-12 09:53:03 -0500

    Add alternative movements

--- GameDev/Assets/Scripts/VistaEscenarioControl.cs
+++ GameDev/Assets/Scripts/VistaEscenarioControl.cs
@@ -7,14 +7,18 @@ public class VistaEscenarioControl : MonoBehaviour {
 	public float speed;
 	// Use this for initialization
 	void Start () {
-		speed = 5.0f;
+		speed = 30.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		/*
+		//Rotar presionando el mouse
 		if (Input.GetMouseButton (0)) {
 			transform.LookAt (target);
 			transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X")*speed);
-		}
+		}*/
+		//Rotar en el mismo punto (por ahora automatico)
+		transform.Rotate(Vector3.up * Time.deltaTime* speed);
 	}
 }
