e96673ed9c362bed9e9e48a8870617ad8ab46b76
HugoIPDev <bairon_myh@yahoo.com.mx>
2018-04-12 08:10:10 -0500

    Add move control

---
+++ GameDev/Assets/Scripts/VistaEscenarioControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VistaEscenarioControl : MonoBehaviour {
+	public Transform target;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			transform.LookAt (target);
+			transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X")*speed);
+		}
+	}
+}
